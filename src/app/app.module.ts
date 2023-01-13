5c52ef75dbd67c4737610f94927b590a8b2d1a83
derenjavadyan <98952880+derenjavadyan@users.noreply.github.com>
2023-01-13 17:33:50 +0400

    installed threeJS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ThreeSceneComponent } from './three-scene/three-scene.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThreeSceneComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/service/main-three-scene.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import * as THREE from 'three';
+
+import myGUI from '../utils/myGUI';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MainThreeSceneService {
+  constructor() {}
+}
